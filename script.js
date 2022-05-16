25c95e34a42af5e9ac577fb466df3dc37b5105cf
Пользователь <ina_gencer@mail.ru>
2022-05-16 21:54:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const next = document.querySelector('#next');
+const back = document.querySelector('#back');
+
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg'];
+
+let i = 0;
+
+next.addEventListener('click', ()=> {
+    i++;
+    if (i > images.length -1) {
+        i = 0;
+    }
+
+    document.querySelector('#picture').src = images[i];
+})
+
+back.addEventListener('click', function() {
+    i--;
+    if (i < 0) {
+        i = images.length - 1;
+    }
+    document.querySelector('#picture').src = images[i];
+})
